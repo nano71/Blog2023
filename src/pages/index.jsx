96417dc7d40a8a211e86bf999b731580d548a91a
nano71 <80870408+nano71@users.noreply.github.com>
2023-10-18 15:57:02 +0800

    initialize

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/public/vite.svg'
+
+function Index() {
+    return (
+        <>
+          <pre>
+              Hi! this is a template of vite + react
+          </pre>
+            <img src={reactLogo} alt=""/>
+            <img src={viteLogo} alt=""/>
+        </>
+    )
+}
+
+export default Index
