6d50ec6f7a20f2a0cf1b15770397f223fc0d7eaf
nano71 <80870408+nano71@users.noreply.github.com>
2024-03-28 15:16:27 +0800

    fix time zone

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,18 +1,6 @@
 import {getArticleContent} from "./http.js";
 import {hiddenError} from "../router/router.jsx";
 
-export function scrollToTop(scrollDuration = 200) {
-    const scrollStep = -window.scrollY / (scrollDuration / 15);
-
-    const scrollInterval = setInterval(() => {
-        if (window.scrollY !== 0) {
-            window.scrollBy(0, scrollStep);
-        } else {
-            clearInterval(scrollInterval);
-        }
-    }, 15);
-}
-
 /**
  * 延迟执行
  * @param {number} delay 延迟多少毫秒
@@ -33,7 +21,7 @@ console.info = function (...args) {
 }
 
 export function formatDatetime(datetime) {
-    return new Date(datetime || Date()).toLocaleString()
+    return new Date(datetime || Date()).toLocaleString("zh-CN", {timeZone: "Asia/Shanghai", hour12: false})
 }
 
 let p = _ => location.pathname
@@ -157,7 +145,7 @@ export const SEOTools = {
      * @param isUseSEOTools 是否使用SEO工具
      * @return {Article}
      */
-    async articleDetailsLoader(articleId,isUseSEOTools = true) {
+    async articleDetailsLoader(articleId, isUseSEOTools = true) {
         console.log(articleId);
         const article = await getArticleContent(parseInt(articleId))
         if (article) {
